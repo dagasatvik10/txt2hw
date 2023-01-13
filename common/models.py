938e187be40a7cae37c2541dd3539af932b9154f
Satvik Daga <dagasatvik10@gmail.com>
2023-01-13 20:01:19 +0530

    refactor: add objects field to BaseModel

--- common/models.py
+++ common/models.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 
 
 class BaseModel(models.Model):
+    objects = models.Manager()
     created_at = models.DateTimeField(db_index=True, default=timezone.now)
     updated_at = models.DateTimeField(auto_now=True)
 
